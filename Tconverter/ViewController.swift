44721b5306f54b0e63ec6c66b5ba8bea59e619ea
Wedlec129 <boryasokolov@gmail.com>
2023-03-25 23:25:59 +0300

    init

--- Tconverter/ViewController.swift
+++ Tconverter/ViewController.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ViewController.swift
 //  Tconverter
 //
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tempCelLaible: UILabel!
+    @IBOutlet weak var tempFarLable: UILabel!
+    
+    //это типо констуктор
+    @IBOutlet weak var slider: UISlider!{
+        didSet{
+            slider.maximumValue=100
+            slider.minimumValue=0
+            slider.value=0
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func sliderChange(_ sender: UISlider) {
+        
+        let tempCelsion=Int(round(slider.value))
+        tempCelLaible.text="\(tempCelsion)ºC"
+        
+        let tempFaring=Int(round((slider.value*9/5)+32))
+        tempFarLable.text="\(tempFaring)ºF"
+        
+        
+        
+    }
+    
+    
+    /*
+     
+     тут самое сложно сделать поворот
+     мы типо делали привязку к экрану C/R и не только картики привязываем но и отступы
+     
+     */
+    
+    
 
 }
 
